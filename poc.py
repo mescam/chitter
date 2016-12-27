dc7f009eb9cd29b8985c78c0f41bbce522b2765c
Jakub Woźniak <j.wozniak@tenesys.pl>
2016-12-28 00:23:16 +0100

    fix

--- poc.py
+++ poc.py
@@ -5,7 +5,7 @@ from cassandra.auth import PlainTextAuthProvider
 auth_provider = PlainTextAuthProvider(username='admin', password='admin13!')
 cluster = Cluster(['node01.aws.mescam.pl'], auth_provider=auth_provider)
 
-session = cluster.connect('Test')
+session = cluster.connect('test')
 print session
 rows = session.execute('SELECT id FROM users')
 for user_row in rows:

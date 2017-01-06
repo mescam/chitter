9c8b928d9fad8d557faa5ce24274d6d608fdce80
Jakub Woźniak <j.wozniak@tenesys.pl>
2017-01-06 18:16:29 +0100

    changed file

--- poc.py
+++ poc.py
@@ -6,9 +6,9 @@ auth_provider = PlainTextAuthProvider(username='admin', password='admin13!')
 cluster = Cluster(['node01.aws.mescam.pl'], auth_provider=auth_provider)
 
 session = cluster.connect('test')
-print session
+print(session)
 rows = session.execute('SELECT id FROM users')
 for user_row in rows:
-    print user_row.id
+    print(user_row.id)
 
 

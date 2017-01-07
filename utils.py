3f263c9ee5a49d0ccfa43d38a158bf4481dcd9f7
Jakub Woźniak <j.wozniak@tenesys.pl>
2017-01-07 23:51:56 +0100

    parse hashtags

---
+++ utils.py
@@ -0,0 +1,12 @@
+import re
+
+_HASHTAG_RE = re.compile(r"\#([A-Za-z0-9]+)", re.M)
+
+def parse_tags(text):
+    return _HASHTAG_RE.findall(text)
+
+if __name__ == '__main__':
+    print(parse_tags("nie wiem co powiedziec #srds2016"))
+    print(parse_tags("kocham #sr #pp"))
+    print(parse_tags("#notice-me-senpai #systemy #rozproszone #nie_wiem"))
+

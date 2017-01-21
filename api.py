e7c500e302c3462a7580f3a9fcfb891b3a0e57be
Jakub Kubiak <jakub.kubiak@yahoo.com>
2017-01-22 00:59:23 +0100

    Added CORS

--- api.py
+++ api.py
@@ -5,12 +5,16 @@ from flask import Flask, jsonify, request
 from flask_restful import Resource, Api
 from flask_jwt_extended import JWTManager
 
+from flask_cors import CORS, cross_origin
+
 from db import Cassandra
 import resources
 
 app = Flask(__name__)
 app.secret_key = os.environ['FLASK_SECRET_KEY']
 app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(days=365)
+
+CORS(app)
 
 api = Api(app)
 jwt = JWTManager(app)
